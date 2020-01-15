24d6fa4b2121a76070416fd7352f7e3e78b04e69
EmanuelCampos <rebortgames@gmail.com>
2020-01-15 14:46:41 -0300

    primeiro envio

---
+++ src/controllers/DevController.js
@@ -0,0 +1,51 @@
+const axios = require('axios')
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+  async index(req, res) {
+    const devs = await Dev.find();
+
+    return res.json(devs)
+  },
+
+  async destroy(req, res) {
+    const del = await Dev.deleteOne({ _id: req.params.id });
+    return res.json(del);
+  },
+
+  async store(req, res) {
+    const { github_username, techs, latitude, longitude } = req.body
+
+    let dev = await Dev.findOne({ github_username })
+
+    if (!dev) {
+
+      const response = await axios.get(`https://api.github.com/users/${github_username}`)
+
+      const { name = login, avatar_url, bio, } = response.data
+
+      const techsArray = parseStringAsArray(techs)
+
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      }
+
+      const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location,
+
+      })
+
+    }
+
+
+    return res.json(dev)
+
+  }
+}
